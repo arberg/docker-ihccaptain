25cf240dc6d141423a22f149ebe9f0e2533a8049
Alex Berg <arberg@gmail.com>
2019-02-21 18:29:40 +0100

    Release script

--- release.sh
+++ release.sh
@@ -8,13 +8,14 @@ git pull
 ./build.sh
 
 # tag it
+TAG="$VERSION"
 git add -A
-git commit -m "version $VERSION"
-git tag -a "$VERSION" -m "version $VERSION"
+git diff-index --quiet HEAD || git commit -m "version $VERSION"
+git diff --quiet HEAD "$TAG" || git tag -a "$TAG" -m "version $VERSION"
 git push
 git push --tags
 
-sudo docker tag $USERNAME/$IMAGE:latest $USERNAME/$IMAGE:$VERSION
+docker tag $USERNAME/$IMAGE:latest $USERNAME/$IMAGE:$VERSION
 # push it
-sudo docker push $USERNAME/$IMAGE:latest
-sudo docker push $USERNAME/$IMAGE:$VERSION
+docker push $USERNAME/$IMAGE:latest
+docker push $USERNAME/$IMAGE:$VERSION
