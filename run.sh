75f71cd3d596f237b59684075bc9a96331337cdb
Alex Berg <arberg@gmail.com>
2019-02-21 18:37:34 +0100

    removed sudo

--- run.sh
+++ run.sh
@@ -1,10 +1,12 @@
 CURRENT_DIR="$( cd "$(dirname "$0")" ; pwd -P )" 
 HOST_PORT=8100
 HOST_SSLPORT=9100
-DOCKER_NAME=ihccaptain
+DOCKER_NAME=IHCCaptain
 
-sudo docker stop $DOCKER_NAME
-sudo docker rm $DOCKER_NAME
+echo "Stopping old if exists"
+docker stop $DOCKER_NAME
+echo "Removing old if exists"
+docker rm $DOCKER_NAME
 # /etc/timezone:ro is a file in the docker but a directory locally on my system
 #sudo docker run -d --name $DOCKER_NAME -p $HOST_PORT:80 -p $HOST_SSLPORT:443 -v $CURRENT_DIR/data:/opt/ihccaptain/data/ -v $CURRENT_DIR/host:/host/ -v "/etc/timezone:/etc/timezone:ro" -v "/etc/localtime:/etc/localtime:ro" arberg/ihccaptain:latest
-sudo docker run -d --name $DOCKER_NAME -p $HOST_PORT:80 -p $HOST_SSLPORT:443 -v $CURRENT_DIR/data:/opt/ihccaptain/data/ -v $CURRENT_DIR/host:/host/ -v "/etc/localtime:/etc/localtime:ro" arberg/ihccaptain:latest
+docker run -d --name $DOCKER_NAME -p $HOST_PORT:80 -p $HOST_SSLPORT:443 -v $CURRENT_DIR/data:/opt/ihccaptain/data/ -v $CURRENT_DIR/host:/host/ -v "/etc/localtime:/etc/localtime:ro" arberg/ihccaptain:latest
